f36f872e6532de5fc316df08421ae484cef67daa
jsgreen90 <42043637+jsgreen90@users.noreply.github.com>
2022-02-14 14:41:27 -0500

    Create Read-AltDataStreams.ps1

---
+++ Read-AltDataStreams.ps1
@@ -0,0 +1,28 @@
+Function Read-AltDataStreams {
+
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true, HelpMessage = 'Please Enter the FULL PATH of the directory you would like to analyze' )]$DirectoryPath,
+        [switch] $Recurse 
+    )
+
+    $StreamFiles = Get-ChildItem $DirectoryPath -Recurse:$Recurse
+    foreach ($File in $StreamFiles)
+    {
+        #Make sure the file is not a directory
+        if (Test-Path $File.FullName -PathType Leaf) 
+        {
+        #Get the streams that are not the :$DATA stream which is present in every NTFS File
+        $streams = Get-Item $File.FullName -Stream * | Where-Object Stream -ne ':$DATA'
+        Write-Host "Alternate Streams for $File" -ForegroundColor Red
+        $streams
+        foreach ($stream in $streams)
+        {
+            #Get the content of the streams
+            Write-Host "Stream Content:" -ForegroundColor Green
+            Get-Content $File.FullName -Stream $stream.Stream            
+        }
+        Write-Host "--------------------------------"
+        }
+    }
+}
