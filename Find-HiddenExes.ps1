b0285541a435b068610352f4558f07c99838771e
jsgreen90 <42043637+jsgreen90@users.noreply.github.com>
2022-02-14 14:43:44 -0500

    Create Find-HiddenExes.ps1

---
+++ Find-HiddenExes.ps1
@@ -0,0 +1,30 @@
+Function Find-HiddenExes {
+
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true, HelpMessage = 'Please Enter the FULL PATH of the directory you would like to analyze' )]$DirPath,
+        [switch] $Recurse
+    )
+    
+    #create whitelist
+    $whitelist = '.exe', '.dll'
+    #grab items in target directory
+    $file_listing = Get-ChildItem -Path $DirPath -Recurse:$Recurse
+    Write-Host("Number of Files/Folders:")$file_listing.count
+    $suspect_files = @()
+    for ($i=0;$i -lt $file_listing.count; $i++)
+    {
+        #for each item make sure its not a directory or whitelisted
+        if ((Test-Path $file_listing[$i] -PathType Leaf) -and ($file_listing[$i].Extension -notin $whitelist))
+        {
+            $exebytes = '{0:X2}' -f (Get-Content $file_listing[$i] -Encoding Byte -ReadCount 4)
+            if($exebytes -eq '4D 5A 90 00')
+            {
+                Write-Host("Found atypical file:")$file_listing[$i]
+                $suspect_files += $file_listing[$i]
+            }
+        }
+    }
+    Write-Host("Number of suspicious files found:")$suspect_files.count
+    Write-Host $suspect_files
+}
