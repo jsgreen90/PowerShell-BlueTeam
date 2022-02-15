1138cbe25cc0e0e7dd17dd5a83d0eee63220c1e0
jsgreen90 <42043637+jsgreen90@users.noreply.github.com>
2022-02-15 08:47:48 -0500

    Create Find-SDDLHiddenServices.ps1

---
+++ Functions/Find-SDDLHiddenServices.ps1
@@ -0,0 +1,17 @@
+<#
+Find Services hidden using SDDL, which can be used for persistance
+https://www.sans.org/blog/red-team-tactics-hiding-windows-services/
+#>
+Function Find-SDDLHiddenServices {
+    #find services hidden using SDDL (some common legitimate hidden services are WUDFRd,WUDFWpdFs,WUDFWpdMtp)
+    $SDDLServices = Compare-Object -ReferenceObject (Get-Service | Select-Object -ExpandProperty Name | % { $_ -replace "_[0-9a-f]{2,8}$" } ) -DifferenceObject (gci -path hklm:\system\currentcontrolset\services |
+    % { $_.Name -Replace "HKEY_LOCAL_MACHINE\\","HKLM:\" } | ? { Get-ItemProperty -Path "$_" -name objectname -erroraction 'ignore' } | 
+    % { $_.substring(40) }) -PassThru | ?{$_.sideIndicator -eq "=>"}
+
+    foreach ($SDDLService in $SDDLServices)
+    {
+        Get-Service -Name $SDDLService | fl *
+    }
+
+    
+}
