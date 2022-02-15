3fd0c6643e790a72c22d3cd2124d3bd1ff2734a6
jsgreen90 <42043637+jsgreen90@users.noreply.github.com>
2022-02-15 08:37:45 -0500

    Create Hash-ActiveServiceDLLs.ps1

---
+++ Functions/Hash-ActiveServiceDLLs.ps1
@@ -0,0 +1,11 @@
+<#
+This will get the hash of the Service DLLs with the DLL Name/Path for active services
+#>
+Function Hash-ActiveServiceDLLs {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true, HelpMessage = 'Please Enter the Algorithm you wish to use with -Algorithm' )]$Algorithm
+    )
+
+    Set-ItemProperty REGISTRY::HKLM\SYSTEM\CurrentControlSet\Services\*\* -ea 0 | where {($_.ServiceDll -ne $null)} | foreach {Get-FileHash -Algorithm $Algorithm $_.ServiceDll}
+}
