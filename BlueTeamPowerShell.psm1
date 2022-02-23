99c8e876921cc1f4b3f9387839a215c3760d7dee
jsgreen90 <42043637+jsgreen90@users.noreply.github.com>
2022-02-23 08:05:25 -0500

    Update BlueTeamPowerShell.psm1

--- BlueTeamPowerShell.psm1
+++ BlueTeamPowerShell.psm1
@@ -542,6 +542,14 @@ function Get-EnrichedProcesses {
   }
 }
 
+function Get-UnsignedDLLs {
+    $a = Get-ChildItem  -Path C:\Windows\* -Include *.dll | Get-AuthenticodeSignature | Where-Object Status -NE "Valid" | Format-List *
+    $b = Get-ChildItem  -Path C:\Windows\System32\* -Include *.dll | Get-AuthenticodeSignature | Where-Object Status -NE "Valid" | Format-List *
+    $c = $a + $b
+    $c
+}
+
 Export-ModuleMember -Function Find-SDDLHiddenServices, Get-ActiveServiceDLLHashes, Get-SuspiciousTasks, Get-Connections, Read-AltDataStreams, 
 Get-LocalMemDump, Get-ParentChildProcess, Get-UserPSHistory, Find-UnsignedDLLs, Find-SusFilterDrivers, Find-HiddenExes, Get-PrivEscInfo,
-Get-SuspiciousPowerShellCommand, Get-DecodedBase64, Get-ProcessTree, Get-ProcessMemory, Show-ProcessMemory, Get-RunningProcessHashes, Get-EnrichedProcesses
+Get-SuspiciousPowerShellCommand, Get-DecodedBase64, Get-ProcessTree, Get-ProcessMemory, Show-ProcessMemory, Get-RunningProcessHashes, Get-EnrichedProcesses,
+Get-UnsignedDLLs
