51fc13acc47c97ee6bf84062f94fdfd4de826464
jsgreen90 <42043637+jsgreen90@users.noreply.github.com>
2022-02-25 09:51:37 -0500

    Update Get-ActiveUnsignedDLLs.ps1

--- Functions/Get-ActiveUnsignedDLLs.ps1
+++ Functions/Get-ActiveUnsignedDLLs.ps1
@@ -3,4 +3,7 @@ Find Drivers used by current processes that are unsigned
 #>
 Function Get-ActiveUnsignedDLLs {
     (gps).Modules.FileName | get-authenticodesignature | ? Status -NE "Valid"
+    #$A=$(foreach ($dll in gps|select -ExpandProperty modules -ea SilentlyContinue){Get-AuthenticodeSignature $dll.FileName |Where-Object Status -NE "Valid"|Select Path});
+    #$B=$(foreach ($dll in $A){Get-FileHash $dll.Path| select Hash -ExpandProperty Hash})|Sort-Object| Get-Unique;
+    #$B
 }
